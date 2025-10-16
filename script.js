5bba0172668b8a2b6c54ee94c059c86b837a4b04
tusmknkamal-max <tusmknkamal@gmail.com>
2025-10-16 13:41:46 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const tombolSapa = document.getElemenById('sapaButton');
+const tombolSapa = document.getElementById('sapaButton');
 tombolSapa.addEventListener('click', function() {
   alert('Halo! Terima kasih sudah berkunjung!');
 });
